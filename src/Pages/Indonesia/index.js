462fecd1b66589bf81beeac253f17f469b92a718
charley-rantung <rantungcharley@gmail.com>
2020-12-02 21:49:25 +0800

    Update Indonesia

--- src/Pages/Indonesia/index.js
+++ src/Pages/Indonesia/index.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState  } from 'react'
+import axios from 'axios'
+import NumberFormat from 'react-number-format'
+import './styles.css'
 
 const Indonesia = () => {
+    const [confirmed, setConfirmed] = useState(0);
+    const [recovered, setRecovered] = useState(0);
+    const [deaths, setDeaths] = useState(0);
+    
+
+    useEffect(() => {
+        axios
+        .get("https://indonesia-covid-19.mathdro.id/api")
+        .then((response) => {
+            setConfirmed(response.data.jumlahKasus);
+            setRecovered(response.data.sembuh);
+            setDeaths(response.data.meninggal);
+        });
+
+    }, [])
     return (
         <div>
-            <p>INDONESIA</p>
+            <div className='report'>
+                <div className='cards'>
+                    <p>Confirmed</p>
+                    <p><NumberFormat value={confirmed} displayType={'text'} thousandSeparator={true}/></p>
+                </div>
+                <div className='cards'>
+                    <p>Recovered</p>
+                    <p><NumberFormat value={recovered} displayType={'text'} thousandSeparator={true}/></p>
+                </div>
+                <div className='cards'>
+                    <p>Deaths</p>
+                    <p><NumberFormat value={deaths} displayType={'text'} thousandSeparator={true}/></p>
+                </div>
+            </div>
+            <p>GLOBAL</p>
         </div>
     )
 }
