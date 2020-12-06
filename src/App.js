f377c511b571acd9703fdf908fe518d1c0d2b52c
charley-rantung <rantungcharley@gmail.com>
2020-12-06 20:21:48 +0800

    undo version

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ const App = () => {
       <div className="ContainerApp">
         <nav>
           <ul>
-            <li><NavLink to="/covid-tracker-web/" style={{color: "#e5e5e5", float: "left", display: "block", textAlign: "center", textDecoration: "none", padding: 14}}> Home </NavLink></li>
-            <li><NavLink to="/covid-tracker-web/global" style={{color: "#e5e5e5", float: "left", display: "block", textAlign: "center", textDecoration: "none", padding: 14}} activeStyle={{fontWeight: "bold", backgroundColor: "#5e0007"}}> Global </NavLink></li>
-            <li><NavLink to="/covid-tracker-web/indonesia" style={{color: "#e5e5e5", float: "left", display: "block", textAlign: "center", textDecoration: "none", padding: 14}} activeStyle={{fontWeight: "bold", backgroundColor: "#5e0007"}}> Indonesia </NavLink></li>
-            <li><NavLink to="/covid-tracker-web/provinsi" style={{color: "#e5e5e5", float: "left", display: "block", textAlign: "center", textDecoration: "none", padding: 14}} activeStyle={{fontWeight: "bold", backgroundColor: "#5e0007"}}> Provinsi </NavLink></li>
+            <li><NavLink to="/" style={{color: "#e5e5e5", float: "left", display: "block", textAlign: "center", textDecoration: "none", padding: 14}}> Home </NavLink></li>
+            <li><NavLink to="/global" style={{color: "#e5e5e5", float: "left", display: "block", textAlign: "center", textDecoration: "none", padding: 14}} activeStyle={{fontWeight: "bold", backgroundColor: "#5e0007"}}> Global </NavLink></li>
+            <li><NavLink to="/indonesia" style={{color: "#e5e5e5", float: "left", display: "block", textAlign: "center", textDecoration: "none", padding: 14}} activeStyle={{fontWeight: "bold", backgroundColor: "#5e0007"}}> Indonesia </NavLink></li>
+            <li><NavLink to="/provinsi" style={{color: "#e5e5e5", float: "left", display: "block", textAlign: "center", textDecoration: "none", padding: 14}} activeStyle={{fontWeight: "bold", backgroundColor: "#5e0007"}}> Provinsi </NavLink></li>
           </ul>
         </nav>
         <Switch>
-          <Route path = "/covid-tracker-web/global"> <Global /> </Route>
-          <Route path = "/covid-tracker-web/indonesia"> <Indonesia /> </Route>
-          <Route path = "/covid-tracker-web/provinsi"> <Provinsi /> </Route>
-          <Route path = "/covid-tracker-web/"> <Home /> </Route>
+          <Route path = "/global"> <Global /> </Route>
+          <Route path = "/indonesia"> <Indonesia /> </Route>
+          <Route path = "/provinsi"> <Provinsi /> </Route>
+          <Route path = "/"> <Home /> </Route>
         </Switch>
       </div>
     </Router>
